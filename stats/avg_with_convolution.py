0113cbbecb02e3c1762b92930eeae64d7cf645fc
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-04-07 16:27:42 -0400

    added quicker function for window-averaging and a correlation scoring function for pearson and spearman

---
+++ stats/avg_with_convolution.py
@@ -0,0 +1,30 @@
+import numpy as np 
+from scipy.ndimage import convolve 
+
+def make_window_avgd_matrix( arr:np.ndarray, window_size:int, convolve_args, ret_type=None ):
+    """
+    Wrapper around vectorized window-average function (see scipy.ndimage.convolve).
+
+    Arguments:
+
+        arr: np.ndarray for which we want to get the window averages.
+
+        window_size: int specifying the size of the square window (should be an odd integer).
+
+        convolve_args:  additional parameters to pass onto scipy.ndimage.convolve 
+                        (see https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.convolve.html)
+                    
+        ret_type: optional specifier for the window-averaged array's dtype (e.g. int or float).
+
+    Returns:
+
+        avgd_arr: np.ndarray copy of the input arr, but after it has been averaged using the specified window size.
+    """
+    # making averaging kernel
+    kernel = np.ones( ( window_size, window_size ) ) / ( window_size**2 )
+    assert np.sum( kernel ) == 1.0
+
+    avgd_arr = convolve( arr, kernel, **convolve_args )
+
+    if ret_type is not None:
+        return avgd_arr.astype( ret_type )
---
+++ stats/score.py
@@ -0,0 +1,50 @@
+import numpy as np 
+
+from scipy.stats import pearsonr, spearmanr 
+
+
+def score_correlation( arr1:np.ndarray, arr2:np.ndarray, triangular="upper", metric="pearson" ):
+    """
+    Wrapper around scipy.stats.pearsonr and scipy.stats.spearmanr to quantify the correlation between two matrices' values.
+
+    Arguments:
+
+        arr1: symmetric or triangular np.ndarray of size M x M. 
+
+        arr2: symmetric or triangular np.ndarray also of size M x M.
+
+        triangular: str indicating whether to compare the upper/lower triangular of the two input arrays. 
+
+        metric: str indicating whether to use the spearman correlation coefficient or pearson correlation coefficient.
+
+    Returns:
+
+        The spearman or pearson correlation coefficient of the values in the two input arrays. 
+    """
+    # argument validation 
+    triangular = triangular.lower()
+    try:
+        assert triangular in [ "upper", "lower" ]
+    except AssertionError as ae:
+        raise AssertionError( f"\ntriangular can be one of 'upper' or 'lower'; you passed: '{triangular}'\n") from ae
+    
+    metric = metric.lower()
+    try:
+        assert metric in [ "pearson", "spearman" ]
+    except AssertionError as ae:
+        raise AssertionError( f"\nmetric can be one of 'pearson' or 'spearman'; you passed: '{metric}'\n") from ae
+
+    inds = None
+    if triangular == "upper":
+        inds = np.triu_indices( arr1.shape[0] )
+        
+    else: # triangular == "lower"
+        inds = np.tril_indices( arr1.shape[0] )
+    
+    flat_arr1 = arr1[ inds ]
+    flat_arr2 = arr2[ inds ]
+
+    if metric == "pearson":
+        return pearsonr( flat_arr1, flat_arr2 )
+    else: # metric == "spearman"
+        return spearmanr( flat_arr1, flat_arr2 )
