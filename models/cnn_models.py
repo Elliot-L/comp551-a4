dbe3a67abe0b9c057192c76b90f195555955ef9f
Elliot Layne <elliot.layne@eigeninnovations.com>
2019-04-10 13:42:45 -0400

    fixing a mix up between basenet and basenetplus

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -54,23 +54,18 @@ class BaseNet(nn.Module):
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
         self.conv1 = nn.Conv2d(1, conv2d1_filters_numbers, conv2d1_filters_size)
-        self.conv1_bn = nn.BatchNorm2d(conv2d1_filters_numbers)
         self.conv2 = nn.Conv2d(conv2d1_filters_numbers, conv2d2_filters_numbers, conv2d2_filters_size)
-        self.conv2_bn = nn.BatchNorm2d(conv2d1_filters_numbers)
         self.conv3 = nn.Conv2d(conv2d2_filters_numbers, 1, conv2d3_filters_size)
 
-
     def forward(self, x):
-        #print("start forwardingf")
         x = self.conv1(x)
-        x = self.conv1_bn(x)
         x = F.relu(x)
         x = self.conv2(x)
-        x = self.conv2_bn(x)
         x = F.relu(x)
         x = self.conv3(x)
         x = F.relu(x)
         return x
+
 
 
 """
@@ -92,16 +87,24 @@ class BaseNetPlus(nn.Module):
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
         self.conv1 = nn.Conv2d(1, conv2d1_filters_numbers, conv2d1_filters_size)
+        self.conv1_bn = nn.BatchNorm2d(conv2d1_filters_numbers)
         self.conv2 = nn.Conv2d(conv2d1_filters_numbers, conv2d2_filters_numbers, conv2d2_filters_size)
+        self.conv2_bn = nn.BatchNorm2d(conv2d1_filters_numbers)
         self.conv3 = nn.Conv2d(conv2d2_filters_numbers, 1, conv2d3_filters_size)
 
+
     def forward(self, x):
+        #print("start forwardingf")
         x = self.conv1(x)
+        x = self.conv1_bn(x)
         x = F.relu(x)
         x = self.conv2(x)
+        x = self.conv2_bn(x)
         x = F.relu(x)
         x = self.conv3(x)
         return x
+
+
 
 
 """
