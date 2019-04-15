e6ccec83d0db732711cf2acd76455e389d962f28
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-04-14 21:15:23 -0400

    added bit for data transform

--- knn_regressor.py
+++ knn_regressor.py
@@ -42,24 +42,40 @@ if __name__ == '__main__':
                         help='num iterations (default: 1)')
     parser.add_argument('--verbose', type=bool, default=True,
                         help='boolean indicator of verbosity')
+    parser.add_argument("--data-transform", type=str, default="None",
+                        help="transform to apply to the data before passing to the CNNs (can be 'mult_cap' or 'log').")
 
     args = parser.parse_args()
 
     print('Loading data...')
 
-    X, y, _ = gather_chromosome_data( args.valid_data_path )
-    # X_test, y_test, _ = gather_chromosome_data(args.test_data_path)
-    # X_train, y_train, _ = gather_chromosome_data(args.train_data_path)
+    # X, y, _ = gather_chromosome_data( args.valid_data_path )
+    X_test, y_test, _ = gather_chromosome_data(args.test_data_path)
+    X_train, y_train, _ = gather_chromosome_data(args.train_data_path)
 
     if args.verbose:
         print('Data loaded')
 
     # # small sample for now to debug, needs to be replaced with real full train / test data
-    X_train = X[:10000]
-    y_train = y[:10000]
+    # X_train = X[:10000]
+    # y_train = y[:10000]
     
-    X_test = X[10000:20000]
-    y_test = y[10000:20000]
+    # X_test = X[10000:20000]
+    # y_test = y[10000:20000]
+
+    if args.data_transform == "mult_cap":
+        # scale by downsample ratio
+        print( "\n>>> Multiplying arrays (not targets) by 16.0 and capping arrays and targets at 100.0\n" )
+        X_train = [ np.clip( arr*16.0, 0.0, 100.0 ) for arr in X_train ]
+        X_valid = [ np.clip( arr*16.0, 0.0, 100.0 ) for arr in X_valid ]
+        y_train = [ max( min( target, 100.0 ), 0.0 ) for target in y_train ] 
+        y_valid = [ max( min( target, 100.0 ), 0.0 ) for target in y_valid ]  
+    elif args.data_transform == "log":
+        print( "\n>>> Appling log( arr + 1.0 ) transform\n" )
+        X_train = [ np.log( arr+1.0 ) for arr in X_train ]
+        X_valid = [ np.log( arr+1.0 ) for arr in X_valid ]
+        y_valid = np.log( np.array( y_valid ) + 1.0 )
+        y_train = np.log( np.array( y_train ) + 1.0 )
 
     X_test = np.array([x.flatten() for x in X_test])
     X_train = np.array([x.flatten() for x in X_train])
